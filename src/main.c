821cae89d118d50b8b6d8face9d49d25f335d3b7
joajfreitas <joaj.freitas@gmail.com>
2016-10-18 00:05:34 +0100

    pseudocode

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+int main(int argc, char *argv[])
+{
+	read_dict();
+	read_pal();
+	write_out();
+	
+	return EXIT_SUCCESS;
+}
+
+void read_dict()
+{
+
